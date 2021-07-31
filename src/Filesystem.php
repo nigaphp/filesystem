64030fe48209315880c9e20f70045224d916b48c
Abass Ben Cheik <cheikabassben@gmail.com>
2021-07-31 20:52:02 +0000

    First release

---
+++ src/Filesystem.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * This file is part of the Nigatedev PHP framework 
+ *
+ * (c) Abass Ben Cheik <abass@todaysdev.com>
+ */
+
+namespace Nigatedev\Filesystem;
+
+/**
+ * The Nigatedev PHP framework filesystem manipulator
+ *
+ * @author Abass Ben Cheik <abass@todaysdev.com>
+ */
+class Filesystem {
+  
+}
