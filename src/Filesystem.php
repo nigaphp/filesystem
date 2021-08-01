5f820d8b8ddc788737f1132dce03aefe0d8d23f8
Abass Ben Cheik <cheikabassben@gmail.com>
2021-08-01 11:17:20 +0000

    Feat: Adding file and directory checker functions

--- src/Filesystem.php
+++ src/Filesystem.php
@@ -1,11 +1,14 @@
 <?php
 /*
- * This file is part of the Nigatedev PHP framework 
+ * This file is part of the Nigatedev PHP framework package 
  *
  * (c) Abass Ben Cheik <abass@todaysdev.com>
  */
 
 namespace Nigatedev\Filesystem;
+
+use Nigatedev\Filesystem\Exceptions\FileNotFoundException;
+use Nigatedev\Filesystem\Exceptions\DirNotFoundException;
 
 /**
  * The Nigatedev PHP framework filesystem manipulator
@@ -14,4 +17,31 @@ namespace Nigatedev\Filesystem;
  */
 class Filesystem {
   
+  /**
+   * @var string $fieName
+   */
+  private string $fieName;
+  
+  public function isFile($fieName): bool
+  {
+    if (!file_exists($fieName)) {
+      throw new FileNotFoundException("Fatal error: {$fieName} file not found");
+    }
+    return true;
+  }
+  
+  public function isFilePut($fieName)
+  {
+    if ($this->isFile($fieName)) {
+    return file_put_contents($fieName);
+    }
+  }
+  
+  public function isDir($dirName)
+  {
+    if (!is_dir($dirName)) {
+      throw new DirNotFoundException("Fatal error: {$dirName} directory not found");
+    }
+    return true;
+  }
 }
